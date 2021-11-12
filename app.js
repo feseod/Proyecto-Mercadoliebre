53002508097f785ba6e14be05a8d139e633f7bd7
feseod <feseod@hotmail.com>
2021-11-11 23:29:20 -0300

    get /

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.listen(process.env.PORT || 3000, ()=>{
     console.log('Servidor corriendo 3000');
 });
 
-app.get('/home', (req,res)=>{
+app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
 });
 
