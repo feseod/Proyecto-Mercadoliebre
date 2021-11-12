45a434796d107335dc3c8300f066352b8b16c271
feseod <feseod@hotmail.com>
2021-11-11 21:58:28 -0300

    primer commit mercadoliebre

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+const path = require('path');
+
+const publicPath = path.resolve (__dirname, './public');
+app.use (express.static(publicPath));
+
+
+app.listen(3000, ()=>{
+    console.log('Servidor corriento 3000');
+});
+
+app.get('/home', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
